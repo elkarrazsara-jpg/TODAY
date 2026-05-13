4f4bd74f77f8e30299a3ba76637895164cc3abf8
Sara ELKARRAZ <saraelkarraz@MacBook-Air-de-Sara.local>
2026-05-13 16:30:29 +0100

    Ajout du formulaire de contact

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("JavaScript fonctionne !");
+const btnMessage = document.getElementById("btnMessage");
+const message = document.getElementById("btnMessage");
+
+btnMessage.addEventListener("click", function () {
+  message.textContent = "Bravo, tu as cliqué sur le bouton !";
+});
+
+const inputNom = document.getElementById("nom");
+const btnNom = document.getElementById("btnNom");
+const resultatNom = document.getElementById("resultatNom");
+
+btnNom.addEventListener("click", function () {
+  resultatNom.textContent = "Bonjour " + inputNom.value;
+});
+
+const contactForm = document.getElementById("contactForm");
+const formResult = document.getElementById("formResult");
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  formResult.textContent = "Message envoyé avec succès !";
+});
